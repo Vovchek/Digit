eceda7df7350bc65bcfdac98f40988784bf7c6a2
Vovchek <Vovchek@inbox.ru>
2026-02-17 07:47:54 +0300

    wip: RedCenterDetector

--- DigitMode/RedCenterDetector.cpp
+++ DigitMode/RedCenterDetector.cpp
@@ -1,6 +1,8 @@
 ﻿#include "DigitMode/RedCenterDetector.h"
 
 #include <array>
+#include <vector>
+#include <algorithm>
 
 // #include "Appdef.h" - MEMO: Appdef.h depends on MFC, do not use here!
 // For now use local ad hoc defines
@@ -23,70 +25,45 @@ std::vector<ExtremumPoint> RedCenterDetector::DetectExtrema(const DigitizationIn
     const int width = input.imageWidth;
     const int height = input.imageHeight;
 
-    std::vector<std::array<int, 4>> bufLine(static_cast<size_t>(height));
-    std::vector<int*> bufPtrs(static_cast<size_t>(height));
-    for (int y = 0; y < height; ++y) {
-        bufLine[static_cast<size_t>(y)] = { -1, -1, -1, -1 };
-        bufPtrs[static_cast<size_t>(y)] = bufLine[static_cast<size_t>(y)].data();
-    }
-
-    std::vector<unsigned char> line(static_cast<size_t>(width));
-    std::vector<unsigned char> invLine(static_cast<size_t>(width));
+    std::vector<uint8_t> line(static_cast<size_t>(width));
+    std::vector<uint8_t> invLine(static_cast<size_t>(width));
 
     for (int y = 0; y < height; ++y) {
-        int left = -1;
-        int right = -1;
-        for (int x = 0; x < width; ++x) {
-            if (input.isVisible(x, y)) {
-                if (left < 0)
-                    left = x;
-                right = x;
-            }
-        }
-
-        if (left < 0 || right < 0) {
-            continue;
-        }
-
-        bufLine[static_cast<size_t>(y)][0] = left;
-        bufLine[static_cast<size_t>(y)][1] = right;
-        bufLine[static_cast<size_t>(y)][2] = -1;
-        bufLine[static_cast<size_t>(y)][3] = -1;
-
         for (int x = 0; x < width; ++x) {
             if (input.isVisible(x, y)) {
                 const int row = (height - 1) - y;
-                const size_t index = static_cast<size_t>(row) * static_cast<size_t>(width) + static_cast<size_t>(x);
-                line[static_cast<size_t>(x)] = input.bitmapData[index];
+                const int index = row * width + x;
+                line[x] = input.bitmapData[index];
             }
             else {
-                line[static_cast<size_t>(x)] = 0;
+                line[x] = 0;
             }
-            invLine[static_cast<size_t>(x)] = line[static_cast<size_t>(x)];
+            invLine[x] = line[x];
         }
 
-        CArray<double, double> redXs;
+        std::vector<double> redXs;
         int nnpolos = 0;
 
         if (input.fringeCenterAs == FC_MAX) {
-            fon_del(line.data(), 0, width - 1);
-            middle(line.data(), width, height, y, bufPtrs.data(), redXs, nnpolos);
+            fon_del_(line.data(), 0, width - 1);
+            redXs = middle_(line.data(), width, y, input.isVisible);
         }
         else if (input.fringeCenterAs == FC_MIN) {
             invert_line(invLine.data(), 0, width - 1);
-            fon_del(invLine.data(), 0, width - 1);
-            middle(invLine.data(), width, height, y, bufPtrs.data(), redXs, nnpolos);
+            fon_del_(invLine.data(), 0, width - 1);
+            redXs = middle_(invLine.data(), width, y, input.isVisible);
         }
         else if (input.fringeCenterAs == FC_MINMAX) {
             fon_del(line.data(), 0, width - 1);
-            middle(line.data(), width, height, y, bufPtrs.data(), redXs, nnpolos);
+            redXs = middle_(line.data(), width, y, input.isVisible);
             invert_line(invLine.data(), 0, width - 1);
             fon_del(invLine.data(), 0, width - 1);
-            middle(invLine.data(), width, height, y, bufPtrs.data(), redXs, nnpolos);
-            SortDouble(redXs);
+            auto tail = middle_(invLine.data(), width, y, input.isVisible);
+			redXs.insert(redXs.end(), tail.begin(), tail.end());
+            std::sort(redXs.begin(), redXs.end());
         }
 
-        for (int i = 0; i < redXs.GetSize(); ++i) {
+        for (int i = 0; i < redXs.size(); ++i) {
             const double redX = redXs[i];
             const int sampleX = static_cast<int>(redX + 0.5);
             double intensity = 0.0;
