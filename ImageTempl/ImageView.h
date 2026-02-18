b1c8ac30f8fbf127d193962d8f11ab48266eaa4e
Vladimir Chekal <vovchek@inbox.ru>
2026-02-18 19:03:34 +0300

    wip: ....

--- ImageTempl/ImageView.h
+++ ImageTempl/ImageView.h
@@ -148,14 +148,6 @@ protected:
     afx_msg void OnUpdateMeasure(CCmdUI* pCmdUI);
     afx_msg void OnFotoSections();
     afx_msg void OnUpdateFotoSections(CCmdUI* pCmdUI);
-    afx_msg void OnExtBounds();
-    afx_msg void OnUpdateExtBounds(CCmdUI* pCmdUI);
-    afx_msg void OnInsBounds();
-    afx_msg void OnUpdateInsBounds(CCmdUI* pCmdUI);
-    afx_msg void OnZoom();
-    afx_msg void OnUpdateZoom(CCmdUI* pCmdUI);
-    afx_msg void OnUndo();
-    afx_msg void OnUpdateUndo(CCmdUI* pCmdUI);
 	afx_msg void OnAutoDigit();
 	afx_msg void OnFCMax();
 	afx_msg void OnUpdateFCMax(CCmdUI* pCmdUI);
@@ -168,45 +160,8 @@ protected:
 	afx_msg void OnUpdateClearDigit(CCmdUI* pCmdUI);
 	afx_msg void OnCalcAproximation();
     afx_msg void OnUpdateCalcAproximation(CCmdUI* pCmdUI);
-	afx_msg void OnAddDot();
-	afx_msg void OnUpdateAddDot(CCmdUI* pCmdUI);
-	afx_msg void OnRemoveDot();
-	afx_msg void OnUpdateRemoveDot(CCmdUI* pCmdUI);
-	afx_msg void OnRemoveFringe();
-	afx_msg void OnUpdateRemoveFringe(CCmdUI* pCmdUI);
-	afx_msg void OnAddZAPSection();
-	afx_msg void OnUpdateAddSection(CCmdUI* pCmdUI);
-	afx_msg void OnDelZAPSection();
-	afx_msg void OnUpdateDelSection(CCmdUI* pCmdUI);
-	afx_msg void OnShiftDotLeft();
-	afx_msg void OnUpdateShiftDotLeft(CCmdUI* pCmdUI);
-	afx_msg void OnShiftDotRight();
-	afx_msg void OnUpdateShiftDotRight(CCmdUI* pCmdUI);
-	afx_msg void OnRenumFringe();
-	afx_msg void OnUpdateRenumFringe(CCmdUI* pCmdUI);
-	afx_msg void OnRenumDot();
-	afx_msg void OnUpdateRenumDot(CCmdUI* pCmdUI);
-	afx_msg void OnNumberMinus();
-	afx_msg void OnUpdateNumberMinus(CCmdUI* pCmdUI);
-	afx_msg void OnNumberPlus();
-	afx_msg void OnUpdateNumberPlus(CCmdUI* pCmdUI);
-    afx_msg BOOL OnEraseBkgnd(CDC* pDC);
-    afx_msg BOOL OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
-    afx_msg void OnSize(UINT nType, int cx, int cy);
-	afx_msg void OnMove(int x, int y);
-    afx_msg void OnKillFocus(CWnd* pNewWnd);
-	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
-	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
-	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
-	afx_msg void OnSetFocus(CWnd* pOldWnd);
-	afx_msg void OnTimer(UINT nIDEvent);
-	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
-	afx_msg void OnRButtonDblClk(UINT nFlags, CPoint point);
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnEditRedo();
-	afx_msg void OnUpdateEditRedo(CCmdUI* pCmdUI);
 };
 
 /////////////////////////////////////////////////////////////////////////////
